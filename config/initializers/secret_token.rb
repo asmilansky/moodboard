10f756c9394ac50574f13521a4228e220e3c3829
Shim <shim@minttigital.com>
2015-04-17 12:43:54 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moodboard::Application.config.secret_key_base = 'ca630bfdd63252175280c50a503a6cac6e7a63fb821c4d7c656eca7ee48cc0103f7008fa5d392c9a929595d856886fae210467ca09929b1c7282bc81e13805e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moodboard::Application.config.session_store :cookie_store, key: '_moodboard_session'
